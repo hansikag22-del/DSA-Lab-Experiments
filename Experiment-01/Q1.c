ea98726bdfaeb023a7cc9a7ef73578e7e4cc4fa5
Hansika Gupta <hansikag22@gmail.com>
2026-02-12 09:42:28 +0530

    Create Q1.c

---
+++ Experiment-01/Q1.c
@@ -0,0 +1,13 @@
+//print a right angled triangle using *
+
+#include <stdio.h>
+int main(){
+    int i,j;
+    for (i=1;i<=5;i++){
+        for (j=1;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
