0a1f5312f903114202adf896f218f1160d219adf
Hansika Gupta <hansikag22@gmail.com>
2026-02-12 09:46:03 +0530

    Implement array reversal using pointers

--- Experiment-01/Q4.c
+++ Experiment-01/Q4.c
@@ -1,13 +1,41 @@
 // reverse an array using pointers.
+// Reverse an array using pointers
 
 #include <stdio.h>
-int main(){
-    int arr[]={1,2,3,4,5};
-    int n= sizeof(arr)/sizeof(arr[0]);
-    reverseArray(arr,n);
-    prin
-    
 
+// Function to reverse array using pointers
+void reverseArray(int *arr, int n) {
+    int *start = arr;          // pointer to first element
+    int *end = arr + n - 1;    // pointer to last element
+    int temp;
 
+    while(start < end) {
+        temp = *start;
+        *start = *end;
+        *end = temp;
 
+        start++;
+        end--;
+    }
 }
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array:\n");
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    reverseArray(arr, n);
+
+    printf("\nReversed array:\n");
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
+
+
